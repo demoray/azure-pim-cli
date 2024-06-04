9affb67a26bc8274e23dd885233798670dba7b7f
Brian Caswell <bcaswell@microsoft.com>
2024-06-04 14:14:44 -0400

    address clippy lints

--- src/az_cli.rs
+++ src/az_cli.rs
@@ -1,12 +1,20 @@
 use anyhow::Result;
 use std::process::Command;
 
+/// Execute an Azure CLI command
+///
+/// # Errors
+/// Will return `Err` if the Azure CLI fails
 fn az_cmd(args: &[&str]) -> Result<String> {
     let output = Command::new("az").args(args).output()?;
     let output = String::from_utf8(output.stdout)?;
     Ok(output.trim().to_string())
 }
 
+/// Get an Oauth token from Azure CLI for the current user
+///
+/// # Errors
+/// Will return `Err` if the Azure CLI fails
 pub fn get_token() -> Result<String> {
     az_cmd(&[
         "account",
@@ -19,6 +27,10 @@ pub fn get_token() -> Result<String> {
     ])
 }
 
+/// Get the user id for the currently logged in user from the Azure CLI
+///
+/// # Errors
+/// Will return `Err` if the Azure CLI fails
 pub fn get_userid() -> Result<String> {
     az_cmd(&[
         "ad",
