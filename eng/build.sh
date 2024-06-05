dc83218f4fefad5995e693198d32b4a6324c5083
Brian Caswell <bcaswell@gmail.com>
2024-06-05 15:02:34 -0400

    Build debian packages (#11)

--- eng/build.sh
+++ eng/build.sh
@@ -2,17 +2,15 @@
 
 set -uvex -o pipefail
 
-BUILD_TARGET=${1:-""}
-BUILD_PROFILE=${2:-"release"}
-
 cd $(dirname ${BASH_SOURCE[0]})/../
 
 which typos || cargo install typos-cli
 
-BUILD_COMMON="--locked --profile ${BUILD_PROFILE}"
-if [ x"${BUILD_TARGET}" != x"" ]; then
-    BUILD_COMMON="${BUILD_COMMON} --target ${BUILD_TARGET}"
+if [[ ${OSTYPE} == "linux-gnu"* ]]; then
+    which cargo-deb || cargo install cargo-deb
 fi
+
+BUILD_COMMON="--locked --profile release"
 
 typos
 cargo clippy ${BUILD_COMMON} --all-targets --all-features -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc -A clippy::module_name_repetitions
@@ -22,3 +20,7 @@ cargo build ${BUILD_COMMON}
 cargo test ${BUILD_COMMON}
 cargo run ${BUILD_COMMON} -- readme > README.md
 git diff --exit-code README.md
+
+if [[ ${OSTYPE} == "linux-gnu"* ]]; then
+    cargo deb
+fi
