3c068bd648c24355e09f880b06ab3fb0fa69f279
Brian Caswell <bcaswell@microsoft.com>
2024-06-14 17:54:57 -0400

    add concurrency

--- src/activate.rs
+++ src/activate.rs
@@ -1,7 +1,12 @@
 use crate::roles::ScopeEntry;
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Result};
 use reqwest::{blocking::Client, StatusCode};
+use retry::{
+    delay::{jitter, Fixed},
+    retry_with_index, OperationResult,
+};
 use serde_json::Value;
+use std::time::Duration;
 use tracing::{debug, info};
 use uuid::Uuid;
 
@@ -18,6 +23,17 @@ fn check_error_response(body: &Value) -> Result<()> {
         return Ok(());
     }
     bail!("unable to elevate: {body:#?}");
+}
+
+macro_rules! try_or_stop {
+    ($e:expr) => {
+        match $e {
+            Ok(x) => x,
+            Err(e) => {
+                return OperationResult::Err(anyhow::Error::from(e));
+            }
+        }
+    };
 }
 
 /// Activates the specified role
@@ -53,22 +69,33 @@ pub fn activate_role(
         }
     });
 
-    let response = Client::new()
-        .put(url)
-        .query(&[("api-version", "2020-10-01")])
-        .bearer_auth(token)
-        .json(&body)
-        .send()?;
+    let func = |_: u64| {
+        let response = try_or_stop!(Client::new()
+            .put(&url)
+            .query(&[("api-version", "2020-10-01")])
+            .bearer_auth(token)
+            .json(&body)
+            .send());
 
-    let status = response.status();
+        let status = response.status();
 
-    if status == StatusCode::BAD_REQUEST {
-        check_error_response(&response.json()?)?;
-        return Ok(None);
-    }
+        if status == StatusCode::TOO_MANY_REQUESTS {
+            return OperationResult::Retry(anyhow!("rate limited"));
+        }
 
-    let body: Value = response.error_for_status()?.json()?;
+        if status == StatusCode::BAD_REQUEST {
+            let body = try_or_stop!(response.json());
+            try_or_stop!(check_error_response(&body));
+            return OperationResult::Ok(None);
+        }
 
-    debug!("body: {status:#?} - {body:#?}");
-    Ok(Some(request_id))
+        let response = try_or_stop!(response.error_for_status());
+        let body: Value = try_or_stop!(response.json());
+
+        debug!("body: {status:#?} - {body:#?}");
+        OperationResult::Ok(Some(request_id))
+    };
+
+    let retries = Fixed::from(Duration::from_secs(5)).map(jitter).take(5);
+    retry_with_index(retries, func).map_err(|e| e.error)
 }
