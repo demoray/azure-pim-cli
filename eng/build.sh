2dd12704370e32d4a2a63ef4ec2dc196f93030f3
Brian Caswell <bcaswell@microsoft.com>
2024-06-21 10:34:04 -0400

    include musl builds for static linux binaries

--- eng/build.sh
+++ eng/build.sh
@@ -2,15 +2,17 @@
 
 set -uvex -o pipefail
 
+BUILD_TARGET=${1:-$(rustc --version --verbose | grep ^host: | cut -d ' ' -f 2)}
+
 cd $(dirname ${BASH_SOURCE[0]})/../
 
 which typos || cargo install typos-cli
 
-if [[ ${OSTYPE} == "linux-gnu"* ]]; then
+if [[ ${BUILD_TARGET} == "x86_64-unknown-linux-gnu" ]]; then
     which cargo-deb || cargo install cargo-deb
 fi
 
-BUILD_COMMON="--locked --profile release"
+BUILD_COMMON="--locked --profile release --target ${BUILD_TARGET}"
 
 typos
 cargo clippy ${BUILD_COMMON} --all-targets --all-features -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc -A clippy::module_name_repetitions
@@ -21,6 +23,6 @@ cargo test ${BUILD_COMMON}
 cargo run ${BUILD_COMMON} -- readme > README.md
 git diff --exit-code README.md
 
-if [[ ${OSTYPE} == "linux-gnu"* ]]; then
-    cargo deb
+if [[ ${BUILD_TARGET} == "x86_64-unknown-linux-gnu" ]]; then
+    cargo deb --target ${BUILD_TARGET}
 fi
