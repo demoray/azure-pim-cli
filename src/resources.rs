83db357c6e58c3222548df71f73cc0b471e6a0eb
Brian Caswell <bcaswell@microsoft.com>
2024-07-11 10:01:02 -0400

    add missing models

---
+++ src/resources.rs
@@ -0,0 +1,47 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::BTreeSet;
+
+use crate::scope::Scope;
+
+#[derive(Serialize, Deserialize, PartialOrd, Ord, PartialEq, Eq, Debug)]
+pub struct ChildResource {
+    pub id: Scope,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub type_: String,
+}
+
+impl ChildResource {
+    pub(crate) fn parse(data: &Value) -> Result<BTreeSet<Self>> {
+        let mut results = BTreeSet::new();
+
+        if let Some(value) = data.get("value") {
+            if let Some(value) = value.as_array() {
+                for entry in value {
+                    let child_resource: ChildResource = serde_json::from_value(entry.clone())?;
+                    results.insert(child_resource);
+                }
+            }
+        }
+
+        Ok(results)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ChildResource;
+    use anyhow::Result;
+    use insta::assert_json_snapshot;
+    use serde_json::{from_str, Value};
+
+    #[test]
+    fn test_child_resource_parse() -> Result<()> {
+        let data: Value = from_str(include_str!("../tests/data/child-resources.json"))?;
+        let result = ChildResource::parse(&data)?;
+        assert_json_snapshot!(result);
+        Ok(())
+    }
+}
