028af1b5f93ab1c27d3c2e6779bf658b90eb4a5c
Brian Caswell <bcaswell@microsoft.com>
2024-07-16 09:30:39 -0400

    add `cargo deny` to CI pipelines

--- eng/build.sh
+++ eng/build.sh
@@ -7,6 +7,7 @@ BUILD_TARGET=${1:-$(rustc --version --verbose | grep ^host: | cut -d ' ' -f 2)}
 cd $(dirname ${BASH_SOURCE[0]})/../
 
 which typos || cargo install typos-cli
+which cargo-deny || cargo install cargo-deny
 
 if [[ ${BUILD_TARGET} == "x86_64-unknown-linux-gnu" ]]; then
     which cargo-deb || cargo install cargo-deb
@@ -15,6 +16,7 @@ fi
 BUILD_COMMON="--locked --profile release --target ${BUILD_TARGET}"
 
 typos
+cargo deny check all --hide-inclusion-graph
 cargo clippy ${BUILD_COMMON} --all-targets --all-features -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc -A clippy::module_name_repetitions
 cargo clippy ${BUILD_COMMON} --tests --all-targets --all-features -- -D warnings
 cargo fmt --check
